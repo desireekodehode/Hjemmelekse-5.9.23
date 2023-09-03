10fee87cfaa94bdcd9e64092adb75182ed8902e2
desireekodehode <desireekodehode1@gmail.com>
2023-09-03 23:01:18 +0200

    *

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hjemmelekse til 5. september 2023")
+
+//// Oppgave 1.////
+
+
+const Butikklapp = [
+        "Brød",
+        "Melk",
+        "Leverpostei",
+        "Knekkebrød",
+        "Frukt",
+        "Nøtter",
+        "Såpe",
+        "Kaffe",
+        "Poteter",
+        "Sjokolade",
+];
+
+Butikklapp.pop(9)
+
+Butikklapp.push("Dopapir")
+Butikklapp.pop(3, 7)
+console.log(Butikklapp.length);
+console.log(Butikklapp)
+Butikklapp[5];
+
+console.log(Butikklapp);
+
+//// Oppgave 1.1 ////
+
+console.log(Butikklapp[5])
+
+//// Oppgave 2////
+
+
+
+
